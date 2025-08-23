1ce87502c6ec0f94fbb5b818037f4de97a870567
Ikun <ikunal03@gmail.com>
2025-08-22 21:55:19 -0700

    Added leads.json and handled file parsing
    Added leads.json to the project for reading and also added file parsing and exception handling for opening files.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
-﻿public class Program
+﻿using System.Text.Json;
+public class Program
 {
     public static int Main(string[] args)
     {
+        //Step 1: Open the input file and read it into a JSON object
+        if (args.Length > 0)
+        {
+            try
+            {
+                string fileName = args[0];
+                if (!File.Exists(fileName))
+                {
+                    throw new FileNotFoundException();
+                }
+                else
+                {
+                    string fileData = File.ReadAllText(fileName);
+                    JsonDocument jsonData = JsonDocument.Parse(fileData);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found: " + e.Message);
+                return 3;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in application: " + e.Message);
+                return 2;
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("No input provided. Please provide input file.");
+            return 1;
+        }
+        //Step 2: Create the data structures that will handle the JSON
+        //object and also handle the duplicates
+
+        //Step 3: Output the record of changes in a JSON file with 
+        //values showing the new values and the old values 
         return 0;
     }
 }
